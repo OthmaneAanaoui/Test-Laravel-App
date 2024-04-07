c872aed834d0bda68e06436da3721334e8bf2ac8
OthmaneAanaoui <47450627+OthmaneAanaoui@users.noreply.github.com>
2024-04-07 23:44:24 +0200

    update services

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -9,19 +9,21 @@ use App\Services\MovieAPIService;
 class MovieController extends Controller
 {
 
-    protected $movieService;
+    protected $movieService; // Declare the MovieAPIService property
 
+    // Constructor to inject MovieAPIService instance
     public function __construct(MovieAPIService $movieService)
     {
-        $this->movieService = $movieService;
+        $this->movieService = $movieService; // Inject MovieAPIService instance
     }
 
-
+    // Method to display the dashboard
     public function index()
     {
+        // Call the storeMoviesFromAPI method from the MovieAPIService
         $movies = $this->movieService->storeMoviesFromAPI();
 
-        //return response()->json($movies);
+        // Return the view with the retrieved movies data
         return view('dashboard', compact('movies'));
     }
 }
